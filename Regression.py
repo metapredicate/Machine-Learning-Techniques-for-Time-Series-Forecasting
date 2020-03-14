3e1e7c8f8af5074aef20bb389ba1ca6f7d0ee575
Eoin Dowling <avieor1@gmail.com>
2020-03-14 21:48:57 +0000

    Changed the Regression.py file so I use it both as a standalone script and to import the file in another script I'm writing to automate tests I've produced.

--- Regression.py
+++ Regression.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot
 import pandas as pd
 import numpy as np
 import datetime
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from datetime import datetime
@@ -16,103 +16,96 @@ def mean_absolute_percentage_error(y_true, y_forecast):
     #     y_true = 0.001 # work around division by zero
     return np.mean(np.abs((y_true - y_forecast) / y_true)) * 100
 
-# create log file for results
-name = "Test "+str(datetime.now())
-pathHead = "./test_results/" + name
-print(pathHead)
-try:
-    os.makedirs(pathHead)
-    print("Directory ", pathHead, " Created ")
-except OSError as e:
-    if(e.errno == errno.EEXIST):
-        print("Directory ", pathHead, " already exists.")
-    else:
-        raisenow = datetime.now()
-pathOutputTail = "/Output-" + name + ".txt"
-pathOutput = Path(pathHead + pathOutputTail)
-text_file = open(str(pathOutput), "w")
-
-
-# Load data (We will be using strict *nix path)
+# load data 
 df = pd.read_csv("./Data/Appliances Energy Usage Prediction/energydata_complete.csv")
-# df.head()
 
 # Convert date column in the appropriate format
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
-# df.head()
 
-# Define the features and target variables
-target = ["Appliances"]
-features = ["lights", "T1", "T2", "T3"]
 
-# Define Train Test Split Ratio (as global variables for the time being)
-train_split = 0.7
-test_split = 0.3
+def run_test(dataframe, target, features, train_split, num_lags):
+    # break conditions
 
-# Obtain the master dataset
-master_df = df[["date"] + target + features].copy()
-# master_df.head()
+    # create log file for results 
+    # if(moniker == ""):
+    #     name = "Test "+str(datetime.now())
+    # else:
+    #     name = "/Test "+str(datetime.now()) + "/" + moniker + "/"
+    name = "Test "+str(datetime.now())
+    pathHead = "./test_results/" + name
+    print(pathHead)
+    try:
+        os.makedirs(pathHead)
+        print("Directory ", pathHead, " Created ")
+    except OSError as e:
+        if(e.errno == errno.EEXIST):
+            print("Directory ", pathHead, " already exists.")
+        else:
+            raisenow = datetime.now()
+    pathOutputTail = "/Output-" + name + ".txt"
+    pathOutput = Path(pathHead + pathOutputTail)
+    text_file = open(str(pathOutput), "w")
+    
+    # Define Train Test Split Ratio
+    test_split = 1 - train_split
 
-# Compute the lags of each and every variable
-newFeatures = []
-num_lags = int(6)
-for i in target + features:
-    for k in range(1,num_lags+1):
-        # Create lags
-        master_df["{}_{}".format(str(i), str(k))] = master_df[i].shift(k)
-        newFeatures.append("{}_{}".format(str(i), str(k)))
-# master_df.head()
+    # Obtain the master dataset
+    master_df = df[["date"] + target + features].copy()
 
-df[target].plot()
+    # Compute the lags of each and every variable
+    newFeatures = []
+    num_lags = int(num_lags)
+    for i in target + features:
+        for k in range(1,num_lags+1):
+            # Create lags
+            master_df["{}_{}".format(str(i), str(k))] = master_df[i].shift(k)
+            newFeatures.append("{}_{}".format(str(i), str(k)))
 
-master_df[["date","Appliances","Appliances_1","Appliances_2"]].head()
+    # Drop Missing Values
+    master_df = master_df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
 
-# Drop Missing Values
-master_df = master_df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
-# master_df.head()
+    # Partition the dataset into Train and Test sets
+    train_records = int(np.round(train_split * master_df.shape[0]))
+    test_records = int(master_df.shape[0]-train_records)
+    X_train = master_df[:train_records][newFeatures].copy()
+    y_train = master_df[:train_records][target].copy()
+    X_test = master_df[-test_records:][newFeatures].copy()
+    y_test = master_df[-test_records:][target].copy()
 
-# Split dataset into train/ test set
-train_records = int(np.round(train_split * master_df.shape[0]))
-test_records = int(master_df.shape[0]-train_records)
+    # Perform Linear Regression on the training data and Predict
+    reg = LinearRegression().fit(X_train, y_train)
+    preds = reg.predict(X_train)
 
-X_train = master_df[:train_records][newFeatures].copy()
-y_train = master_df[:train_records][target].copy()
+    # Infomation written to the log files (for test purposes)
+    text_file.write("Number of Lags = "+str(num_lags)+"\n")
+    text_file.write("Features we are using = "+str(features)+"\n")
 
-X_test = master_df[-test_records:][newFeatures].copy()
-y_test = master_df[-test_records:][target].copy()
+    # Measure the model performance on the train set
+    resultant_train_MAE = mean_absolute_error(y_train, preds)
+    text_file.write("resulant_train_MAE "+str(resultant_train_MAE)+ "\n")
+    resultant_train_MAPE = mean_absolute_percentage_error(y_train, preds)
+    text_file.write("resultant_train_MAPE "+str(resultant_train_MAPE)+ "\n")
+    print("Performance on Training Set (MAE) : {:.3f} ".format(resultant_train_MAE))
+    print("Performance on Training Set (MAPE): {:.3f} %".format(resultant_train_MAPE ))
+    # Measure the model performance on the test set
+    preds_test = reg.predict(X_test)
 
-X_train.shape
-y_test.shape
+    resultant_test_MAE = mean_absolute_error(y_test, preds_test)
+    text_file.write("resulant_test_MAE "+str(resultant_test_MAE)+ "\n")
+    resultant_test_MAPE = mean_absolute_percentage_error(y_test, preds_test)
+    text_file.write("resulant_test_MAPE "+str(resultant_test_MAPE)+ "\n")
+    print("Performance on Test Set (MAE) : {:.3f} ".format(resultant_test_MAE))
+    print("Performance on Test Set (MAPE): {:.3f}  %".format(resultant_test_MAPE))
 
-# Training a Regression Model
-reg = LinearRegression().fit(X_train, y_train)
-preds = reg.predict(X_train)
-
-# Measure the model performance on the train set
-resultant_train_MAE = mean_absolute_error(y_train, preds)
-text_file.write("resulant_train_MAE "+str(resultant_train_MAE)+ "\n")
-resultant_train_MAPE = mean_absolute_percentage_error(y_train, preds)
-text_file.write("resultant_train_MAPE "+str(resultant_train_MAPE)+ "\n")
-print("Performance on Training Set (MAE) : {:.3f} ".format(resultant_train_MAE))
-print("Performance on Training Set (MAPE): {:.3f} %".format(resultant_train_MAPE ))
-# Measure the model performance on the test set
-preds_test = reg.predict(X_test)
-
-resultant_test_MAE = mean_absolute_error(y_test, preds_test)
-text_file.write("resulant_test_MAE "+str(resultant_test_MAE)+ "\n")
-resultant_test_MAPE = mean_absolute_percentage_error(y_test, preds_test)
-text_file.write("resulant_test_MAPE "+str(resultant_test_MAPE)+ "\n")
-print("Performance on Test Set (MAE) : {:.3f} ".format(resultant_test_MAE))
-print("Performance on Test Set (MAPE): {:.3f}  %".format(resultant_test_MAPE))
-
-# plot the actual data and the predicted data
-pyplot.clf()
-pyplot.plot(y_test)
-pyplot.plot(preds_test, color='red')
-pathGraphTail = "/" + name + ".png"
-pathGraph = Path(pathHead+pathGraphTail)
-pyplot.savefig(pathGraph)
-pyplot.show()
+    # Plotting is not so useful to us right now until we think of a fix.
+    # plot the actual data and the predicted data
+    # pyplot.clf()
+    # pyplot.plot(y_test)
+    # pyplot.plot(preds_test, color='red')
+    # pathGraphTail = "/" + name + ".png"
+    # pathGraph = Path(pathHead+pathGraphTail)
+    # pyplot.savefig(pathGraph)
+    # pyplot.show()
 
 # try:
 #     pyplot.clf()
@@ -127,3 +120,10 @@ pyplot.show()
 # with open('errors.txt', "a") as errors:
 #     s = "" + name + " " + str(resultant_test_MAPE) + "\n"
 #     errors.write(s)
+
+if __name__ == "__main__":
+    # Define the features and target variables
+    target = ["Appliances"]
+    features = ["lights", "T1", "T2", "T3"]
+    run_test(df, target, features, 0.7, 6)
+    pass
