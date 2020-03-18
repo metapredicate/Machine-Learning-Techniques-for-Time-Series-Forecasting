a68d0066dbf9c76c9c68ec26c854eeb8253e0960
dowlinry <dowlinry@tcd.ie>
2020-03-18 14:55:02 +0000

    Changed data structure from list to dictionary

--- UI/Log_Book_Class.py
+++ UI/Log_Book_Class.py
@@ -2,12 +2,13 @@
 class log_book:
 
     def __init__(self, model, dataset, date):
-        self.log_entry_list = []
+        self.log_entry_dict = {}
         self.newRequestWidget
         self.selectedLogEntry
+        self.log_entry_count = 0
 
     def append_log_entry(new_entry):
-        log_entry_list.append(new_entry)
+        log_entry_dict[log_entry_count] = new_entry
 
     def init_select_log_entry():
         
