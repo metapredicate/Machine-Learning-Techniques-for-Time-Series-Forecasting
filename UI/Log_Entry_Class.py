1852bcd5bd2c38d7edd0c41a9541617864fe55b3
liamby <60388361+liamby@users.noreply.github.com>
2020-03-20 17:05:02 +0000

    Updated graphing methods

--- UI/Log_Entry_Class.py
+++ UI/Log_Entry_Class.py
@@ -1,25 +1,43 @@
 #object which will contain all the information about the forecast
 import pandas as pd
 import plotly.graph_objects as go
+import math
 
 class Log_Entry:
 
 
     #Constructor
-    def __init__(self, model, dataset, date ):
+    def __init__(self, model, dataset, date, ratio ):
         self.model = model
         self.dataset = dataset
         self.date = date
+        self.ratio = ratio
         self.notes = 'Please write your notes here'
         self.status = 'training'
         self.status_color = '#ff0000'
-        self.data_frame = pd.read_csv(dataset)
-        self.meta_data = self.data_frame.describe()
-        self.columns = self.data_frame.columns
-        self.training_graph = self.graph_data()
-        self.forecasting_graph = self.graph_data()
-        print('log entry created')
+        self.input_data_frame = pd.read_csv(dataset)
+        self.trainng_data_frame = self.input_data_frame
+        self.forecast_data_frame = self.input_data_frame
+        self.split_data_frame()
+        self.meta_data = self.input_data_frame.describe()
+        self.columns = self.input_data_frame.columns
+        self.training_graph = self.graph_data(self.trainng_data_frame)
+        self.forecasting_graph = self.graph_data(self.forecast_data_frame)
+        print('Log Entry Created')
 
+    #splits the input data_frame into a training_data_frame and
+    #forecast_data_frame according to the ratio
+    def split_data_frame(self):
+
+        length = len(self.input_data_frame.index)
+        print("***************************INPUT SIZE***************************************")
+        print(length)
+        self.trainng_data_frame = self.input_data_frame.truncate(before=0, after=math.ceil((length)*self.ratio))
+        print("************************TRAINING DATAFRAME****************************************8 ")
+        print(self.trainng_data_frame)
+        self.forecast_data_frame = self.input_data_frame.truncate(before=math.ceil((length)*self.ratio), after=length)
+        print("**********************FORECASTING DATAFRAME*********************************")
+        print(self.forecast_data_frame)
 
     #Sets status of log entry to 'ready'
     def set_status_to_ready(input):
@@ -35,7 +53,7 @@ class Log_Entry:
 
     #Creates meta_data list
     def generate_meta_data(data_frame):
-        self.meta_data = data_frame.describe()
+        self.meta_data = input_data_frame.describe()
 
 
     #Adds to meta_data list
@@ -43,14 +61,16 @@ class Log_Entry:
         self.meta_data.append(new_meta_data)
 
 
-    #Creates a graph and decides which graphing method to call
-    def graph_data(self):
+    #Takes in a log_entry and dataframe so it knows what its trying to graph.
+    #Creates a graph and decides which graphing method to call.
+    def graph_data(self, data_frame):
 
         graph = go.Figure()
+        
         if self.dataset == 'energydata_complete.csv':
-            graph =self.graph_energydata_complete(graph)
+            graph =self.graph_energydata_complete(graph, data_frame)
         elif self.dataset == 'monthly-sunspots.csv' :
-            graph = self.graph_monthly_sunspots(graph)
+            graph = self.graph_monthly_sunspots(graph, data_frame)
         else:
             print('***ERROR***: CANT READ DATA FILE')
 
@@ -58,14 +78,14 @@ class Log_Entry:
 
 
     #Graphs energydata_complete
-    def graph_energydata_complete(self, graph):
+    def graph_energydata_complete(self, graph, data_frame  ):
         #Add lines
         for x in self.columns:
             if x != 'date':
                 if x == 'Energy Usage':
-                    graph.add_trace(go.Scatter(x=self.data_frame.date, y=self.data_frame[x], name = x, line_color='deepskyblue'))
+                    graph.add_trace(go.Scatter(x=data_frame.date, y=data_frame[x], name = x, line_color='deepskyblue'))
                 else:
-                    graph.add_trace(go.Scatter(x=self.data_frame.date, y=self.data_frame[x], name = x, line_color='#a6a6a6'))
+                    graph.add_trace(go.Scatter(x=data_frame.date, y=data_frame[x], name = x, line_color='#a6a6a6'))
         #update trace
         graph.add_trace
 
@@ -79,11 +99,11 @@ class Log_Entry:
 
 
     #Graphs monthly_sunspots
-    def graph_monthly_sunspots(self, graph):
+    def graph_monthly_sunspots(self, graph, data_frame):
         #Add lines
         for x in self.columns:
             if x != 'Month':
-                graph.add_trace(go.Scatter(x=self.data_frame.Month, y=self.data_frame[x], name = x, line_color='#4db8ff'))
+                graph.add_trace(go.Scatter(x=data_frame.Month, y=data_frame[x], name = x, line_color='#4db8ff'))
         #update trace
         graph.add_trace
         #Update range slider
