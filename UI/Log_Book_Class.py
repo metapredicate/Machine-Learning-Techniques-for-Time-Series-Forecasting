870c899ace2d518c0880fe0b95f82e9fa10b86dc
liamby <60388361+liamby@users.noreply.github.com>
2020-03-21 21:49:13 +0000

    Updated log book class

--- UI/Log_Book_Class.py
+++ UI/Log_Book_Class.py
@@ -1,14 +1,19 @@
 #object which will contain all the log entries
-class log_book:
+import dash_html_components as html
 
-    def __init__(self, model, dataset, date):
-        self.log_entry_dict = {}
-        self.newRequestWidget
-        self.selectedLogEntry
-        self.log_entry_count = 0
+class Log_Book:
 
-    def append_log_entry(new_entry):
-        log_entry_dict[log_entry_count] = new_entry
+    def __init__(self,):
+        self.log_entry_array = []
+        self.button_array = []
+        self.selected_log_entry = 'none'
+        print('Log book created', flush=True)
 
-    def init_select_log_entry():
-        
+    def append_log_entry(self, new_entry):
+        self.log_entry_array.append(new_entry)
+        self.button_array.append(self.log_entry_to_button(new_entry))
+        print('New log entry appended to logbook', flush=True)
+
+    def log_entry_to_button(self, new_entry):
+        print('New button created', flush=True)
+        return html.Button(new_entry.dataset)
