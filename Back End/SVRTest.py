9197df075c7679dd4e4245e007b381fc3cf8310c
duffyc13 <60395760+duffyc13@users.noreply.github.com>
2020-04-16 11:53:11 +0100

    Update SVRTest.py

--- Back End/SVRTest.py
+++ Back End/SVRTest.py
@@ -2,9 +2,9 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import datetime
 from datetime import datetime
 import os
-import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
